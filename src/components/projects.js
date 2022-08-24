46157340d44279a861a994cc31f6d8acfee52093
K. Kote <27905113+kkote@users.noreply.github.com>
2022-08-24 03:54:43 -0500

    style projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,57 +12,113 @@ const Skills = props => {
     const skillsUsed = props.skills || ['HTML', 'CSS', 'Bootstrap']
   
     return (
-      <ul style={styles.lang}>
+      <ul style={styles.skillGroup}>
         {skillsUsed.map((item, i) => (
-          <li style={styles.langItem} key={i}>{item} </li>
+          <li style={styles.skillItem} key={i}>{item} </li>
         ))}
       </ul>
     )
   }
 
 const styles = {
-    wrapper: {
+  section: {
+    marginTop: '5em',
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: '1em',
+    textTransform: 'uppercase',
+  },
+  wrapper: {
 	    padding: '2rem 0',
     },
     grid: {
 	    display: 'grid',
         alignItems: 'center',
-        //gridTemplateColumns: 'repeat(3, 1fr)',
+        gridTemplateColumns: 'repeat(3, 1fr)',
         gridTemplateRows: '8fr',
         gap: '1.2rem 1.2rem',
     },
-    item: {
-/*         width: '100%',
-	    height: '100%',
-	    overflow: 'hidden',*/
-	    boxShadow: '0 1px 6px 0 rgba(0, 0, 0, 0.11)', 
-        display: 'flex',
-        flexDirection: 'column',
-        marginTop: '3rem',
-        maxWidth: '25rem',
-        width: '100%',
+    
+    projectWrapper: {
+      boxShadow: '0 5px 15px lightgray',
+      display: 'flex',
+      MozBoxPack: 'justify',
+      justifyContent: 'space-between',
+      MozBoxAlign: 'center',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      position: 'relative',
+      height: '100%',
+      padding: '2rem 1.75rem',
+      borderRadius: 'var(--border-radius)',
+      backgroundColor: 'var(--light-navy)',
+      transition: 'var(--transition)',
+      overflow: 'auto',
     },
-    content: {
-        display: 'flex',
-	    flexDirection: 'column',
-	    justifyContent: 'space-between',
-	    height: '100%',
+    header: {
+	    width:"100%"
     },
+    projectLinks: {
+      display: 'flex',
+      MozBoxPack: 'justify',
+      justifyContent: 'space-between',
+      MozBoxAlign: 'center',
+      alignItems: 'center',
+      /* marginBottom: '35px', */
+    },
+    linksRight: {
+      display: 'flex',
+      MozBoxAlign: 'center',
+      alignItems: 'center',
+      marginRight: '-10px',
+      color: 'var(--light-slate)',
+    },
+    link: {
+      display: 'flex',
+      MozBoxPack: 'center',
+      justifyContent: 'center',
+      MozBoxAlign: 'center',
+      alignItems: 'center',
+      padding: '5px 7px',
+    },
+
+    projectName: {
+      margin: '0px 0px 10px',
+      color: 'var(--lightest-slate)',
+      fontSize: '20px',
+    },
+    projectDesc: {
+      color: 'var(--light-slate)',
+      fontSize: '17px',
+    },
+
     imgProject: {
-        height: '30px',
-		width: '30px',
-		filter: 'saturate(100%) hue-rotate(-75deg)',
+      height: '30px',
+		  width: '30px',
+		  filter: 'saturate(100%) hue-rotate(-75deg)',
     },
-    aProject: {
-        paddingRight: '.5rem',
-    },
-    icons: {
-        display: 'flex',
-	    justifyContent: 'flex-end',
-    },
+    skillGroup: {
+      display: 'flex',
+      alignItems: 'flex-end',
+      MozBoxFlex: '1',
+      flexGrow: '1',
+      flexWrap: 'wrap',
+      padding: '0px',
+      margin: '20px 0px 0px',
+      listStyle: 'none',
+  },
+  skillItem: {
+    marginRight: '15px',
+    marginBottom: "0px",
+    fontSize: 'var(--fz-xxs)',
+    lineHeight: '1.75',
+    fontFamily: 'var(--font-mono)',
+    listStyle: 'none',
+},
+
     lang: {
 /*         display: 'inline-flex',*/
-
 	    width: '100%',
 	    justifyContent: 'flex-start',
 	    listStyleType: 'none',
@@ -78,77 +134,82 @@ const styles = {
 		marginBottom: '0px',
 		color: '#333f58',
     },
-    card: {
-        padding: '1rem',
-	    background: '#fff',
-	    height: '250px',
-    },
 }
 
 const links = [
   {
     name: "Stocks-data",
     url: "https://github.com/kkote/stocks-data",
-    description: "Historical stock data stored in a database.",
+    description: "Historical stock data stored in a database",
     skills: ["Python", "MySQL", "AWS", "EOD Historical Data API"],
   },
   {
     name: "Portfolio-v2",
     url: "https://github.com/kkote/portfolio-v2",
-    demoUrl: "https://github.com/kkote/weather-app",
-    description: "links, demos, languages used on projects",
+    demoUrl: "",
+    description: "Previous portfolio site, using API for project data",
     skills: ["React", "Gatsby", "GraphQL", "GitHub API"],
   },
   {
     name: "Library-react",
     url: "https://github.com/kkote/library-react",
-    demoUrl: "https://github.com/kkote/weather-app",
+    demoUrl: "https://kkote.github.io/library-react/",
     description: "Catalog your books",
     skills: ["JavaScript", "React", "Google Books API"],
   },
   {
     name: "Weather-app",
     url: "https://github.com/kkote/weather-app",
-    demoUrl: "https://github.com/kkote/weather-app",
-    description: "Temperature based apparel suggestions ",
+    demoUrl: "https://weather-style-guide.herokuapp.com/",
+    description: "Temperature based apparel suggestions",
     skills: ["JavaScript", "React", "Open Weather API", "Material-UI"],
+  },
+  {
+    name: "bf-calc-react",
+    url: "https://github.com/kkote/bf-calc-react",
+    demoUrl: "https://kkote.github.io/bf-calc-react/",
+    description:  "Calculates fitness stats using body measurements",
+    skills: ["JavaScript", "React", "Bootstrap"],
+  },
+  {
+    name: "library-jquery",
+    url: "https://github.com/kkote/library-jquery",
+    demoUrl: "https://kkote.github.io/library-jquery/",
+    description: "Previous version of library-react",
+    skills: ["JavaScript", "jQuery", "Google Books API"],
   },
 ]
 
 
 const Projects = () => (
-  <div style={styles.wrapper} id="projects">
-    <h2>Projects</h2>
+  <section id="projects" style={styles.section}>
+    <h2 style={styles.title}>Projects</h2>
     <div style={styles.grid}>
-      {/* {edges.map(({ node }) => ( */}
-      {/* <Item key={node.id} as="div"> */}
       {links.map(link => (
-      <div key={link.name}  style={styles.item} >
-        <div style={styles.card}>
-          <div style={styles.content}>
-            
-              <div style={styles.icons}>
-              {link.demoUrl ? (
-                <a style={styles.aProject}  href={link.demoUrl} target="_blank" rel="noopener noreferrer" >
+        <div key={link.name} style={styles.projectWrapper} >
+          <header style={styles.header}>
+            <div style={styles.projectLinks}>
+              <div></div>
+              <div style={styles.linksRight}>
+                {link.demoUrl ? (
+                  <a style={styles.link} href={link.demoUrl} target="_blank" rel="noopener noreferrer" >
                     <img style={styles.imgProject} src={openIcon} alt="Open Icon" />
-                </a>) : ( <a href="" /> )}
-                <a style={styles.aProject} href={link.url} target="_blank" rel="noopener noreferrer">
-                   <img style={styles.imgProject} src={githubIcon} alt="GitHub Icon" /> 
+                  </a>) : (<a href="" />)}
+                <a style={styles.link} href={link.url} target="_blank" rel="noopener noreferrer">
+                  <img style={styles.imgProject} src={githubIcon} alt="GitHub Icon" />
                 </a>
               </div>
-
-              <h4>{link.name}</h4>
-              <p>{link.description}</p>
-            
-            <div>
-              <Skills skills={link.skills} />
-            </div> 
-          </div>
+            </div>
+            <h3 style={styles.projectName}>{link.name}</h3>
+            <div style={styles.projectDesc}><p>{link.description}</p></div>
+          </header>
+          <footer>
+            <Skills skills={link.skills} />
+          </footer>
         </div>
-      </div>
-       ))} 
+      ))}
     </div>
-  </div>
+  </section>
 )
 
 export default Projects
